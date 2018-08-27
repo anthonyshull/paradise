fafddd3abaf009ef417c8d7954b2a500f2a9a786
Anthony Shull <shull.anthony@gmail.com>
2018-08-27 16:05:54 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+  name="Paradise",
+  version="0.1",
+  description="A functional library.",
+  author="Anthony Shull",
+  author_email="shull.anthony@gmail.com",
+  url="https://github.com/anthonyshull/paradise",
+  packages=["paradise"]
+)
